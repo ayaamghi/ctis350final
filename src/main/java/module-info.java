a6c18eff2c4a4ca7b16cb6825013c951189739fe
ayaamghi <ayaamghi@gmail.com>
2025-04-21 14:59:03 -0400

    tesat change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,21 @@
-module edu.guilford.final {
-    requires javafx.controls;
-    requires javafx.fxml;
+module edu.guilford.ctisfinal {
+    requires javafx.graphics;
 
-
-    opens edu.guilford.final to javafx.fxml;
-    exports edu.guilford.final;
+        requires javafx.controls;
+        requires javafx.fxml;
+    requires java.desktop;
+    requires ai.djl.api;
+    requires ai.djl.tokenizers;
+    requires org.apache.commons.csv;
+    requires java.naming;
+    exports edu.guilford.ctisfinal.Backend;
+    opens edu.guilford.ctisfinal.Backend to ai.djl.api;
+    exports edu.guilford.ctisfinal.GUI;
+    opens edu.guilford.ctisfinal.GUI to javafx.fxml;
+    exports edu.guilford.ctisfinal.GUI.TabControllers;
+    opens edu.guilford.ctisfinal.GUI.TabControllers to javafx.fxml;
+    exports edu.guilford.ctisfinal.Backend.Inference;
+    opens edu.guilford.ctisfinal.Backend.Inference to ai.djl.api;
+    exports edu.guilford.ctisfinal.Backend.Inference.Embeddings;
+    opens edu.guilford.ctisfinal.Backend.Inference.Embeddings to ai.djl.api;
 }
